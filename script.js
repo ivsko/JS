0d97eba1e9eb702a92a62ac2996e9f59300f841f
Ivan Kolev <88732318+ivsko@users.noreply.github.com>
2025-05-17 12:20:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function calculate() {
+  const m1 = parseFloat(document.getElementById("m1").value);
+  const s1 = parseFloat(document.getElementById("s1").value);
+  const s2 = parseFloat(document.getElementById("s2").value);
+  const mg1 = parseFloat(document.getElementById("mg1").value);
+  const mg2 = parseFloat(document.getElementById("mg2").value);
+
+  if (isNaN(m1) || isNaN(s1) || isNaN(s2) || isNaN(mg1)|| isNaN(mg2)) {
+    alert("Моля, попълни всички стойности!");
+    return;
+  }
+  let mg =0;
+  if(mg2>=18){mg = 35}
+  else{mg = 33 }
+const result =((0.76*(s1-s2)+mg1)/(0.044*mg))*m1;
+
+
+
+  document.getElementById("result").textContent = result.toFixed(1);
+}
